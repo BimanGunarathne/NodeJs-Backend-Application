b65ef80a43f5750d12fd416553e3fd8a8128b4d4
Biman Gunarathne <44740959+BimanGunarathne@users.noreply.github.com>
2023-09-13 15:45:40 +0530

    Add a dependency

--- Database/database.js
+++ Database/database.js
@@ -16,3 +16,23 @@ const userSchema = new mongoose.Schema({
     requird: true,
   },
 });
+
+const weatherSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  temperature: Number,
+});
+
+const User = mongoose.model("User", userSchema);
+const WeatherSchema = mongoose.model("WeatherData", weatherSchema);
+
+module.exports = {
+  User,
+  WeatherSchema,
+};
--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const axios = require("axios");
+const { User, WeatherSchema } = require("./Database/database");
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
