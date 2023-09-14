a3c77e5f3002652247419c0b76b3b804b3482eed
Biman Gunarathne <44740959+BimanGunarathne@users.noreply.github.com>
2023-09-14 07:01:36 +0530

    modified

--- app.js
+++ app.js
@@ -87,6 +87,6 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: "Something went wrong!" });
 });
 
-app.listen(PORT, () => {
+var server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
